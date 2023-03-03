2195924051ac9aee32ffd554c0add0e8b3c21861
ShirAmit96 <92683819+ShirAmit96@users.noreply.github.com>
2023-03-03 18:20:49 +0200

    Update voidPointer.cpp

--- voidPointer.cpp
+++ voidPointer.cpp
@@ -5,7 +5,7 @@ int main()
 {
 	int a = 10;
 	void* ptr = &a;
-  cout << *ptr << endl;// compiler error
+	cout << *ptr << endl;// compiler error
 	cout << *(int *)ptr << endl;//the right way to print
 
 	return 0;
