298cd08975f0ca2fff146a340c2ca2a501939c72
ShirAmit96 <92683819+ShirAmit96@users.noreply.github.com>
2023-03-03 18:16:55 +0200

    Update voidPointer.cpp

--- voidPointer.cpp
+++ voidPointer.cpp
@@ -1,1 +1,17 @@
 
+
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 10;
+	void* ptr = &a;
+  cout << *ptr << endl;// compiler error
+	cout << *(int *)ptr << endl;//the right way to print
+
+	return 0;
+}
+
