d92dff6c1f908dce091dea3c821948ac5f2592fb
ShirAmit96 <92683819+ShirAmit96@users.noreply.github.com>
2023-03-07 15:16:11 +0200

    Create multipleInheritence.cpp

---
+++ multipleInheritence.cpp
@@ -0,0 +1,66 @@
+# include <iostream>
+using namespace std;
+
+// two classes that inherits the same class:
+
+//class A {  public: virtual void f() {
+//    cout<<"A::f()"<<endl;
+//}
+//};
+//class B:public A
+//{
+//public:
+//    void g(){
+//        cout<<"B::g()"<<endl;
+//    }
+//
+//    using B:: void f(){
+//        cout<<"B::f()"<<endl;
+//    }
+//};
+//class C:public A
+//{
+//public:
+//    void h(){
+//        cout<<"C::h()"<<endl;
+//    }
+//};
+//
+//
+//int main()
+//{
+//    A* a= new B();
+//    a->f();
+//  //  c->f();
+////    c.f();
+////    c.g();
+////    c.h();
+////    A* pA=&c;
+////    B* pB=&c;
+////    pB->h();
+////    pA->f();
+////    pB->g();
+//
+//}
+
+// one class that inherits two classes:
+class Father{
+public:
+    void hug(int i){
+        cout<<"father hugs"<<endl;
+    }
+};
+class Mother{
+public:
+    void hug(int i, int j){
+        cout<<"mother hugs"<<endl;
+    }
+};
+class Son: public Father, public Mother{
+
+};
+
+int main(){
+    Son joe;
+    joe.hug(5, 7);//compilation error
+}
